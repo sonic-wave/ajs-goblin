b2d08c7ed647a592caaeb8e3967bba788074a9d9
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-12-20 15:05:47 +0300

    change files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# DEMON SLAYER
+https://sonic-wave.github.io/events/
 
+[![Build status](https://ci.appveyor.com/api/projects/status/hveygxxchhwq6xay?svg=true)](https://ci.appveyor.com/project/sonic-wave/events)
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 const goblin = document.querySelectorAll('.cell');
-const goblinImage = document.createElement('img');
+// const goblinImage = document.createElement('img');
 
-goblinImage.src = '/src/goblin.png'
+// goblinImage.src = '../img/goblin.png'
 
 let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let randomNum = 0;
@@ -11,10 +11,13 @@ const newInterval = setInterval(() => {
     if(!numArray.length) {
         numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     }
+    goblin.forEach((item) => {
+        if (item.classList.contains('field-item-img')) {
+            item.classList.remove('field-item-img');
+        };
+})
     randomNum = Math.floor(Math.random() * numArray.length);    
     temp = numArray[randomNum];
     numArray = numArray.filter((element) => element !== temp);
-    console.log(`temp: ${temp}`);
-    console.log(`numArray: ${randomNum}`)
-    goblin[temp].appendChild(goblinImage);
+    goblin[temp].classList.add('field-item-img');
 }, 1000)
